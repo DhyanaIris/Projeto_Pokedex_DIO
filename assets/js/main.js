c4481f144101a58019c45e54608e9b40d19dc4cb
Dhyana Iris <dhyanairis@gmail.com>
2023-12-29 18:59:37 -0300

    iniciando javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("Sucesso!");
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
