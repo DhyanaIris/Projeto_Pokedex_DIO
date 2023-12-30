bd7cb2310b6686f92ebb95912d613dbf2bd39401
Dhyana Iris <dhyanairis@gmail.com>
2023-12-30 12:11:42 -0300

    arrumando formatação e criando a lista de pokemons no js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,32 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+  return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <section class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </section>
+        </li>
+    `;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
 fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon);
+    }
+  })
+  .catch((error) => console.error(error));
