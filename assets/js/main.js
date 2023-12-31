02e8cc14c7b6bc51d8d749b33aac7a9618036b3c
Dhyana Iris <dhyanairis@gmail.com>
2023-12-31 16:40:50 -0300

    criando classe pokemon  e usando nosso proprio objeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,13 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
-  );
-}
-
 function convertPokemonToLi(pokemon) {
   return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <section class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join("")}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join("")}
                 </ol>
-                <img src="${pokemon.sprites.other.dream_world.front_default}"
+                <img src="${pokemon.photo}"
                     alt="${pokemon.name}">
             </section>
         </li>
